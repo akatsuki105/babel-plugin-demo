47a515ba3a048b248aa15a530fb2f1ec6c2f9c13
pokemium <bluejapan73+dev@gmail.com>
2021-05-19 16:25:10 +0900

    complete!

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+import generate from '@babel/generator';
+import { parse } from '@babel/parser';
+import traverse from '@babel/traverse';
+
+const main = () => {
+  const ast = parse(`
+const a = 1
+let b = 2
+`);
+
+  traverse(ast, {
+    VariableDeclaration: (path) => {
+      path.node.kind = 'var';
+    },
+  });
+  console.log(generate(ast).code);
+};
+
+main();
